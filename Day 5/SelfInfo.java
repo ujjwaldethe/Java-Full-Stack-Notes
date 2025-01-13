d4a8b27d6bea452a7fe5f86ae132cc26bb3c3151
ujjwal dethe <ujwaldethe518@gmail.com>
2025-01-13 14:33:15 +0530

    Self Introduction Using Java Code

---
+++ Day 5/SelfInfo.java
@@ -0,0 +1,12 @@
+class SelfInfo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name : Ujjwal Shriram Dethe");
+		System.out.println("Age : 21");
+		System.out.println("Collage Name : Kcts Late G N Sapkal Collage Of Engg");
+		System.out.println("Education : BE Engineering");
+		System.out.println("Stream :  Computer Engineering");
+		System.out.println("Thank You");
+	}
+}
